4e3d7e8c12c9aa737d982eac8adbd73b485acadf
mark-summerfield <mark@qtrac.eu>
2023-10-02 10:30:31 +0100

    started

--- debsearch.go
+++ debsearch.go
@@ -4,13 +4,13 @@
 package debsearch
 
 import (
-    "fmt"
-    _ "embed"
-    )
+	_ "embed"
+	"fmt"
+)
 
 //go:embed Version.dat
 var Version string
 
 func Hello() string {
-    return fmt.Sprintf("Hello debsearch v%s", Version)
+	return fmt.Sprintf("Hello debsearch v%s", Version)
 }
--- debsearch_test.go
+++ debsearch_test.go
@@ -1,13 +1,13 @@
 package debsearch
 
 import (
-    "testing"
+	"testing"
 )
 
 func Test001(t *testing.T) {
-    expected := "Hello debsearch v0.1.0\n"
-    actual := Hello()
-    if actual != expected {
-        t.Errorf("expected %q, got %q", expected, actual)
-    }
+	expected := "Hello debsearch v0.1.0\n"
+	actual := Hello()
+	if actual != expected {
+		t.Errorf("expected %q, got %q", expected, actual)
+	}
 }
